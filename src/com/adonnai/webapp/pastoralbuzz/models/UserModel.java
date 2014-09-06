d3a0b30ebcdf39c2dd8143cc90e4d4ea7360b052
dhara <hearbeat.horror@gmail.com>
2014-09-06 15:37:53 +0530

    updated
    Signed-off-by: dhara <hearbeat.horror@gmail.com>

--- src/com/adonnai/webapp/pastoralbuzz/models/UserModel.java
+++ src/com/adonnai/webapp/pastoralbuzz/models/UserModel.java
@@ -1,12 +1,39 @@
 package com.adonnai.webapp.pastoralbuzz.models;
 
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
 public class UserModel {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@PrimaryKey
+	@Persistent
 	private int id;
+	@Persistent
 	private String userName;
+	@Persistent
 	private String password;
+	@Persistent
 	private String firstName;
+	@Persistent
 	private String lastName;
+	@Persistent
 	private String telephoneNumber;
+	@Persistent
+	private boolean isAdmin;
+	
+	@Persistent
+	@ManyToOne
+	@JoinColumn(table="Address",name="userId")
+	private AddressModel address;
 	
 	public int getId() {
 		return id;
@@ -43,5 +70,17 @@ public class UserModel {
 	}
 	public void setTelephoneNumber(String telephoneNumber) {
 		this.telephoneNumber = telephoneNumber;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public AddressModel getAddress() {
+		return address;
+	}
+	public void setAddress(AddressModel address) {
+		this.address = address;
 	}
 }
